659ff7d814fa8dcb4278b22f0728b4d43f937eab
TurnstileAI <turnstileai@outlook.com>
2026-06-21 20:49:10 +0100

    Add exports and types to index.ts

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+export { TurnstileAI } from "./client";
+export {
+  TurnstileError,
+  TurnstileAuthError,
+  TurnstileRequestError,
+  TurnstileVerificationError,
+} from "./errors";
+
+export type {
+  TurnstileConfig,
+  InferenceOptions,
+  RunRecord,
+  LedgerCheckpoint,
+  VerificationResult,
+  ProviderHealth,
+  UsageOverview,
+  RouteMode,
+  LedgerMode,
+  TurnstileApiErrorBody,
+} from "./types";
