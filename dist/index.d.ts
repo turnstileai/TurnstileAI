021563a6f5e5ceebcf571114000d8798c1730659
xbd x <turnstileai@outlook.com>
2026-06-22 19:44:39 +0100

    Update dist exports

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,8 +1,8 @@
 export { TurnstileAI } from "./client";
 export { ReceiptsResource } from "./receipts";
-export { TurnstileError, TurnstileAuthError, TurnstileRequestError, TurnstileVerificationError, TurnstileAIError, TurnstileAIAuthError, TurnstileAIAPIError, TurnstileAIVerificationError, } from "./errors";
-export type { TurnstileConfig, TurnstileAIConfig, InferenceOptions, ChatMessage, ChatCompletionRequest, ChatCompletionResponse, ChatCompletionChoice, ComputeReceipt, ReceiptAnchor, ReceiptVerificationResponse, RunRecord, LedgerCheckpoint, VerificationResult, ProviderHealth, UsageOverview, RouteMode, LedgerMode, TurnstileApiErrorBody, } from "./types";
+export { TurnstileAIError, TurnstileAIAuthError, TurnstileAIAPIError, TurnstileAIVerificationError, } from "./errors";
 export { verifyReceiptSignature, verifyInclusionProof, buildSignaturePayload, } from "./verifier";
+export type { TurnstileAIConfig, ChatMessage, ChatCompletionRequest, ChatCompletionResponse, ChatCompletionChoice, ComputeReceipt, ReceiptAnchor, ReceiptVerificationResponse, OfflineVerificationResult, ReceiptPublicKey, } from "./types";
 export type { ReceiptSignaturePayload } from "./verifier";
 export type { InclusionProof } from "./receipts";
 //# sourceMappingURL=index.d.ts.map
